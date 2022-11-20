b5c1c02b06327fc6f940b7f9ffd2741cbf5e39bd
JooJo <sjak0101@gmail.com>
2022-11-20 08:09:56 +0000

    added react environment variable and connected firebase authentication

--- src/Pages/Appointment/AvailableAppoinments.js
+++ src/Pages/Appointment/AvailableAppoinments.js
@@ -14,7 +14,7 @@ const AvailableAppoinments = ({date}) => {
     },[])
     return (
         <div>
-            <h4 className='text-xl text-secondary text-center'>Available Appointments on {format(date, 'PP')}</h4>
+            <h4 className='text-xl text-secondary text-center my-12'>Available Appointments on {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
                     services.map(service=><Service
