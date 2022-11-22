e938b4c7bb9eaec964b27de1af2ce9dfb67689dc
JooJo <sjak0101@gmail.com>
2022-11-22 03:10:39 +0000

    added some function in login page like loading spinner, error showing

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
 
-    if (user) {
+    let signInError;
+
+    if (loading || gLoading) {
+        return <Loading></Loading>
+    }
+
+    if (error || gError) {
+        signInError = <p className='text-red-500'><small>{error?.message || gError?.message}</small></p>
+    }
+
+
+    if (gUser) {
         console.log(user);
     }
 
     const onSubmit = data => {
         console.log(data);
+        signInWithEmailAndPassword(data.email, data.password);
     }
     return (
         <div className='flex justify-center items-center h-screen'>
@@ -44,7 +63,7 @@ const Login = () => {
                             <label class="label">
                                 {errors.email?.type === 'required' && <span className='label-text-alt text-red-500'>{errors.email.message}</span>}
                                 {errors.email?.type === 'pattern' && <span className='label-text-alt text-red-500'>{errors.email.message}</span>}
-                                
+
                             </label>
                         </div>
                         <div class="form-control w-full max-w-xs">
@@ -69,15 +88,11 @@ const Login = () => {
                             <label class="label">
                                 {errors.password?.type === 'required' && <span className='label-text-alt text-red-500'>{errors.password.message}</span>}
                                 {errors.password?.type === 'minLength' && <span className='label-text-alt text-red-500'>{errors.password.message}</span>}
-                                
+
                             </label>
                         </div>
 
-
-
-
-
-                        
+                        {signInError}
 
                         <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
                     </form>
